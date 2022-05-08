d251456ef79b3247c898b92ab71a891e4812f9a3
AlirezaDaqiq <alireza.daghigh1999@gmail.com>
2022-05-08 05:20:27 +0430

    add some custom load gen

--- loadtest/custom_load.py
+++ loadtest/custom_load.py
@@ -1,31 +1,48 @@
 import asyncio
+import logging
 import pathlib
 import sys
-
+import threading
 import aiohttp
 from aiohttp import ClientSession, ClientConnectorError
 
 
-async def fetch(url: str, session: ClientSession, **kwargs):
+async def fetch(url: str, i, session: ClientSession, index, **kwargs):
     try:
+        print("start ", i, index)
         resp = await session.request(method="GET", url=url, ssl=False, **kwargs)
 
     except ClientConnectorError:
         return (url, 404)
+    print("end ", i)
     return (url, resp.status)
 
-async def make_request(url,count,  **kwargs):
+
+async def make_request(url, count, idx, **kwargs):
     async with ClientSession(trust_env=True) as session:
         tasks = []
-        for _ in range(count):
-            tasks.append(fetch(url, session, **kwargs))
-        results = await asyncio.gather(*tasks)
-        for result in results:
-            print(f'{result[1]} - {str(result[0])}')
+        result = await fetch(url, count, session, idx, **kwargs)
+        print(result)
+
+
+async def generator(url, index, **kwargs):
+    for i in range(10):
+        await asyncio.gather(make_request(url, i, index))
+
+
+def send_request_thread(url):
+    asyncio.run(generator("https://realpython.com/", url))
+
 
 if __name__ == "__main__":
     assert sys.version_info >= (3, 7), "Script requires Python 3.7+."
     here = pathlib.Path(__file__).parent
-    asyncio.run(make_request("https://stackoverflow.com/", 10))
+    while True:
+        pass
 
 
+
+    for index in range(3):
+        logging.info("Main    : create and start thread %d.", index)
+        x = threading.Thread(target=send_request_thread, args=(index,))
+        x.start()
