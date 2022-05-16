d460f066e8511fc2ddcddfb8e133690f2794bd55
Saeid Ghafouri <s.ghafouri@qmul.ac.uk>
2022-05-16 19:01:26 +0000

    midway implementing stress  pipelines

--- pipelines/11-complete-example/stress-custom.py
+++ pipelines/11-complete-example/stress-custom.py
@@ -12,54 +12,92 @@ import random
 import math
 # import plot_builder as pb
 
-final_data = []
+class StressGenerator:
+    def __init__(self, url, is_dataset, data_path = None):
+        self.final_data = []
+        self.url = url
+        self.is_dataset = is_dataset
+        self.data_path = data_path
+        self.idx = 0
 
 
-def nextParam():
-    return random.randint(100, 500)
+
+    def nextParam():
+        return random.randint(100, 500)
+        return generator()
 
 
 
-def nextTimeReqeust(rateParameter):
-    return -math.log(1.0 - random.random()) / rateParameter
+    def nextTimeReqeust(rateParameter):
+        return -math.log(1.0 - random.random()) / rateParameter
 
 
-async def fetch(url: str, session: ClientSession, index, **kwargs):
-    start_time = int(round(time.time() * 1000))
-    try:
-        resp = await session.post(url=url,
-                                    json={"data":{"ndarray":[[1.0, 2.0, 5.0, 6.0]]}},
-                                     ssl=False, **kwargs)
-        end_time = int(round(time.time() * 1000))
-        final_data.append((index, resp.status, 1, start_time, end_time, end_time - start_time))
-    except ClientConnectorError:
-        return (url, 404)
-    return (url, resp.status)
+    async def fetch(self, session: ClientSession, index, **kwargs):
+        start_time = int(round(time.time() * 1000))
+        try:
+            resp = await session.post(url=self.url,
+                                        json={"data":{"ndarray":[[1.0, 2.0, 5.0, 6.0]]}},
+                                        ssl=False, **kwargs)
+            end_time = int(round(time.time() * 1000))
+            self.final_data.append((index, resp.status, 1, start_time, end_time, end_time - start_time))
+        except ClientConnectorError:
+            return (self.url, 404)
+        return (self.url, resp.status)
 
 
-async def make_request(url, idx, **kwargs):
-    headers = {
-            'Content-Type': 'application/json',
-            
-        }
-    async with ClientSession(headers=headers,trust_env=True) as session:
-        tasks = []
-        result = await fetch(url, session, idx, **kwargs)
-        await session.close()
+    async def make_request(self, idx, **kwargs):
+        headers = {
+                'Content-Type': 'application/json',
+                
+            }
+        async with ClientSession(headers=headers,trust_env=True) as session:
+            tasks = []
+            result = await fetch(session, idx, **kwargs)
+            await session.close()
 
 
-async def generator(url, index, **kwargs):
-    await asyncio.gather(make_request(url, index))
+    async def generator(url, index, **kwargs):
+        await asyncio.gather(make_request(url, index))
 
 
-def send_request_thread(idx):
-    asyncio.run(generator("http://localhost:32000/seldon/default/sklearn-prof-daqiq/api/v1.0/predictions", idx))
+    def send_request_thread(idx):
+        asyncio.run(generator(idx))
+
+    def run():
+        seconds = str(time.ctime()).split(":")[2][:2]
+        start = seconds
+        counter = 0
+        param = nextParam()
+        threads = []
+        while True:
+            if str(time.ctime()).split(":")[2][:2] != seconds:
+                seconds = str(time.ctime()).split(":")[2][:2]
+                counter += 1
+                param = nextParam()
+
+                print("time is " + seconds + " param is "+str(param))
+                if counter > 180:
+                    break
+            sender = threading.Thread(target=send_request_thread, args=(idx,))
+            idx += 1
+            threads.append(sender)
+            sender.start()
+            next_event = nextTimeReqeust(param)
+            time.sleep(round(next_event, 4))
+
+        for th in threads:
+            th.join()
 
 
-idx = 0
+
+
+
 
 
 def main():
+    assert sys.version_info >= (3, 7), "Script requires Python 3.7+."
+    here = pathlib.Path(__file__).parent
+    print("start load testing...")
     global idx
     seconds = str(time.ctime()).split(":")[2][:2]
     start = seconds
@@ -81,22 +119,13 @@ def main():
         sender.start()
         next_event = nextTimeReqeust(param)
         time.sleep(round(next_event, 4))
-
+    print("wait till all conncetions done...")
     for th in threads:
         th.join()
-
-
-if __name__ == "__main__":
-    assert sys.version_info >= (3, 7), "Script requires Python 3.7+."
-    here = pathlib.Path(__file__).parent
-    print("start load testing...")
-    main()
-    print("wait till all conncetions done...")
-    time.sleep(5)
+        time.sleep(5)
     df = pd.DataFrame(final_data, columns=['id', 'status', 'isok', 'start', 'end', 'latency'])
     time.sleep(1)
     print(df)
     print("finish works")
     df.to_csv("users.csv")
-    
-    # while True:
+
