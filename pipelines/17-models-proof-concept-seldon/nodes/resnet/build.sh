3295e7432ffd36ccce8664c2deb8875305857208
Saeid Ghafouri <s.ghafouri@qmul.ac.uk>
2022-07-25 11:55:43 +0000

    deleted unnecessary files

--- pipelines/17-models-proof-concept-seldon/nodes/resnet/build.sh
+++
@@ -1,6 +0,0 @@
-REPO_NAME=concept-proof
-TAG_NAME=resnet
-IMAGE_NAME=$REPO_NAME:$TAG_NAME && \
-docker build --tag=$IMAGE_NAME . && \
-docker tag $IMAGE_NAME sdghafouri/$IMAGE_NAME && \
-docker push sdghafouri/$IMAGE_NAME
