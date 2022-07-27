64b92b9a8c9741b136de2c623c86b65b002ad89a
Saeid Ghafouri <s.ghafouri@qmul.ac.uk>
2022-07-27 16:43:16 +0000

    profiling code semi-polished

--- pipelines/issue-unexpected-behavior/nodes/video-openaplr/build.sh
+++
@@ -1,4 +0,0 @@
-IMAGE_NAME=inferline:video-openaplr && \
-docker build --tag=$IMAGE_NAME . && \
-docker tag $IMAGE_NAME sdghafouri/$IMAGE_NAME && \
-docker push sdghafouri/$IMAGE_NAME
--- pipelines/issue-unexpected-behavior/nodes/video-resnet-vehicle/build.sh
+++
@@ -1,4 +0,0 @@
-IMAGE_NAME=inferline:video-resnet-vehicle && \
-docker build --tag=$IMAGE_NAME . && \
-docker tag $IMAGE_NAME sdghafouri/$IMAGE_NAME && \
-docker push sdghafouri/$IMAGE_NAME
