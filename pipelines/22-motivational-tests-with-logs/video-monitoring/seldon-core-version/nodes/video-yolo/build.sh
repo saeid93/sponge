66847d5d0cf5d799e7b910adf961906991007080
Saeid Ghafouri <s.ghafouri@qmul.ac.uk>
2022-08-25 11:38:29 +0000

    added google contaienr registry

--- pipelines/22-motivational-tests-with-logs/video-monitoring/seldon-core-version/nodes/video-yolo/build.sh
+++ pipelines/22-motivational-tests-with-logs/video-monitoring/seldon-core-version/nodes/video-yolo/build.sh
@@ -1,4 +1,10 @@
-IMAGE_NAME=motivational-tests-with-logs:video-yolo && \
-docker build --tag=$IMAGE_NAME . && \
-docker tag $IMAGE_NAME sdghafouri/$IMAGE_NAME && \
-docker push sdghafouri/$IMAGE_NAME
+REPOS=(
+    sdghafouri
+    gcr.io/hale-ivy-335012)
+IMAGE_NAME=motivational-tests-with-logs:video-yolo
+docker build --tag=$IMAGE_NAME .
+for REPO in ${REPOS[@]}
+do
+    docker tag $IMAGE_NAME $REPO/$IMAGE_NAME
+    docker push $REPO/$IMAGE_NAME
+done
