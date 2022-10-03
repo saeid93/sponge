f24bcfaacc3721072954dd2b464a96eb2c76e532
Saeid Ghafouri <s.ghafouri@qmul.ac.uk>
2022-10-03 01:27:06 +0000

    completed all nlp pipelines

---
+++ pipelines/mlserver-prototype/TODO-paper-nlp/seldon-core-version/nodes/nlp-sum/client-sync.py
@@ -0,0 +1,56 @@
+import os
+import requests
+from pprint import PrettyPrinter
+pp = PrettyPrinter(indent=4)
+import json
+from mlserver.types import InferenceResponse
+from mlserver.codecs.string import StringRequestCodec
+import pathlib
+
+# single node inferline
+# gateway_endpoint="localhost:32000"
+# deployment_name = 'nlp-sum'
+# namespace = "default"
+
+# endpoint = f"http://{gateway_endpoint}/seldon/{namespace}/{deployment_name}/v2/models/infer"
+
+# single node inferline
+gateway_endpoint="localhost:8080"
+model='nlp-sum'
+endpoint = f"http://{gateway_endpoint}/v2/models/{model}/infer"
+
+def send_requests(endpoint, data):
+    payload = {
+        "inputs": [
+            {
+            "name": "text_inputs",
+            "shape": [1],
+            "datatype": "BYTES",
+            "data": [json.dumps(data)],
+            "parameters": {
+                "content_type": "str"
+            }
+            }
+        ]
+    }
+    response = requests.post(endpoint, json=payload)
+    return response
+
+PATH = pathlib.Path(__file__).parent.resolve()
+
+with open(os.path.join(PATH, 'input-sample.json'), 'r') as openfile:
+    data = json.load(openfile)
+
+data = [data]
+
+# sync version
+results = []
+for data_ins in data:
+    response = send_requests(endpoint, data_ins)
+    results.append(response)
+
+pp.pprint(results[0])
+inference_response = InferenceResponse.parse_raw(response.text)
+raw_json = StringRequestCodec.decode_response(inference_response)
+output = json.loads(raw_json[0])
+pp.pprint(output)
