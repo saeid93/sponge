95b625e754715bce78581a68022dbf5c46c8fdd3
Saeid Ghafouri <s.ghafouri@qmul.ac.uk>
2022-11-16 01:38:53 +0000

    adding the hack pipeliens

--- pipelines/mlserver-hack/gpu-prototype/2-paper-audio-sent/seldon-core-version/nodes/audio/build.sh
+++ pipelines/mlserver-hack/gpu-prototype/2-paper-audio-sent/seldon-core-version/nodes/audio/build.sh
@@ -1,7 +1,7 @@
 REPOS=(
     sdghafouri
     gcr.io/hale-ivy-335012)
-IMAGE_NAME=audio-sent-pipelines-mlserver:audio
+IMAGE_NAME=audio-sent-pipelines-mlserver-hack:audio
 PYTHON_ENV=central
 mlserver build . -t $IMAGE_NAME
 for REPO in ${REPOS[@]}
