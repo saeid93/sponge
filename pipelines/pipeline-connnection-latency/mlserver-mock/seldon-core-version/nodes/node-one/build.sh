0364a841816fcf6baea84996cf229ee7491a130c
Saeid Ghafouri <s.ghafouri@qmul.ac.uk>
2022-12-25 08:20:02 +0800

    first example of making containers with forked mlserver

--- pipelines/pipeline-connnection-latency/mlserver-mock/seldon-core-version/nodes/node-one/build.sh
+++ pipelines/pipeline-connnection-latency/mlserver-mock/seldon-core-version/nodes/node-one/build.sh
@@ -2,7 +2,7 @@ REPOS=(
     sdghafouri)
 IMAGE_NAME=grpc-pipeline:node-one
 # mlserver build . -t $IMAGE_NAME
-docker build . --tag=$IMAGE_NAME
+DOCKER_BUILDKIT=1 docker build . --tag=$IMAGE_NAME
 for REPO in ${REPOS[@]}
 do
     docker tag $IMAGE_NAME $REPO/$IMAGE_NAME
