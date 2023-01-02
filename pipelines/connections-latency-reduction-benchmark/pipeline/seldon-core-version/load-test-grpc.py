3a450225cc301e4f185cdd29a0102dd15714cfe6
Saeid Ghafouri <s.ghafouri@qmul.ac.uk>
2023-01-02 00:46:16 +0000

    latency reduction on pipelines completed

--- pipelines/connections-latency-reduction-benchmark/pipeline/seldon-core-version/load-test-grpc.py
+++ pipelines/connections-latency-reduction-benchmark/pipeline/seldon-core-version/load-test-grpc.py
@@ -4,12 +4,12 @@ from barazmoon import MLServerAsyncGrpc
 from barazmoon import Data
 from datasets import load_dataset
 import asyncio
+import json
 import time
 import numpy as np
 
-
-load = 10
-test_duration = 30
+load = 1
+test_duration = 5
 variant = 0
 platform = 'seldon'
 mode = 'exponential'
@@ -17,13 +17,13 @@ mode = 'exponential'
 # single node inference
 if platform == 'seldon':
     endpoint = "localhost:32000"
-    deployment_name = 'node-one'
-    model = 'node-one'
+    deployment_name = 'mock-pipeline'
+    model = None
     namespace = "default"
     metadata = [("seldon", deployment_name), ("namespace", namespace)]
 elif platform == 'mlserver':
     endpoint = "localhost:8081"
-    model = 'node-one'
+    model = None
     metadata = []
 
 data_type = 'audio-bytes'
@@ -81,21 +81,24 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 # Through away initial seconds results
+responses = responses[3:]
+
+# Through away initial seconds results
 # responses = responses[3:]
 
-# # roundtrip latency
-# roundtrip_lat = []
-# for sec_resps in responses:
-#     for resp in sec_resps:
-#         request_times = resp['times']['request']
-#         sending_time = request_times['arrival'] - request_times['sending']
-#         roundtrip_lat.append(sending_time)
-# fig, ax = plt.subplots()
-# ax.plot(np.arange(len(roundtrip_lat)), roundtrip_lat)
-# ax.set(xlabel='request id', ylabel='roundtrip latency (s)', title=f'roundtrip latency, total time={round((time.time() - start_time))}')
-# ax.grid()
-# fig.savefig(f"grpc-compressed-audio-{platform}_variant_{variant}-roundtrip_lat-load-{load}-test_duration-{test_duration}.png")
-# plt.show()
+# roundtrip latency
+roundtrip_lat = []
+for sec_resps in responses:
+    for resp in sec_resps:
+        request_times = resp['times']['request']
+        sending_time = request_times['arrival'] - request_times['sending']
+        roundtrip_lat.append(sending_time)
+fig, ax = plt.subplots()
+ax.plot(np.arange(len(roundtrip_lat)), roundtrip_lat)
+ax.set(xlabel='request id', ylabel='roundtrip latency (s)', title=f'roundtrip latency, total time={round((time.time() - start_time))}')
+ax.grid()
+fig.savefig(f"{platform}_variant_{variant}-roundtrip_lat-load-{load}-test_duration-{test_duration}.png")
+plt.show()
 
 # # sending time
 # start_times = []
@@ -108,7 +111,7 @@ import numpy as np
 # ax.plot(np.arange(len(start_times)), start_times)
 # ax.set(xlabel='request id', ylabel='sending time (s)', title=f'load tester sending time, total time={round((time.time() - start_time))}')
 # ax.grid()
-# fig.savefig(f"grpc-compressed-audio-{platform}_variant_{variant}-sending_time-load-{load}-test_duration-{test_duration}.png")
+# fig.savefig(f"{platform}_variant_{variant}-sending_time-load-{load}-test_duration-{test_duration}.png")
 # plt.show()
 
 # # server arrival time
@@ -122,22 +125,23 @@ import numpy as np
 # ax.plot(np.arange(len(server_arrival_time)), server_arrival_time)
 # ax.set(xlabel='request id', ylabel='server arrival time (s)', title=f'Server recieving time of requests, total time={round((time.time() - start_time))}')
 # ax.grid()
-# fig.savefig(f"grpc-compressed-audio-{platform}_variant_{variant}-server_arrival_time_from_start-load-{load}-test_duration-{test_duration}.png")
+# fig.savefig(f"{platform}_variant_{variant}-server_arrival_time_from_start-load-{load}-test_duration-{test_duration}.png")
 # plt.show()
 
 # server arrival latency
-server_arrival_latency = []
-for sec_resps in responses:
-    for resp in sec_resps:
-        times = resp['times']
-        server_recieving_time = times['models'][model]['arrival'] - times['request']['sending']
-        server_arrival_latency.append(server_recieving_time)
-fig, ax = plt.subplots()
-ax.plot(np.arange(len(server_arrival_latency)), server_arrival_latency)
-ax.set(xlabel='request id', ylabel='server arrival latency (s)', title=f'Server recieving latency, total time={round((time.time() - start_time))}')
-ax.grid()
-fig.savefig(f"custom-{platform}-load-{load}-test_duration-{test_duration}.png")
-plt.show()
+# model = 'mock-one'
+# server_arrival_latency = []
+# for sec_resps in responses:
+#     for resp in sec_resps:
+#         times = resp['times']
+#         server_recieving_time = times['models'][model]['arrival'] - times['request']['sending']
+#         server_arrival_latency.append(server_recieving_time)
+# fig, ax = plt.subplots()
+# ax.plot(np.arange(len(server_arrival_latency)), server_arrival_latency)
+# ax.set(xlabel='request id', ylabel='server arrival latency (s)', title=f'Server recieving latency, total time={round((time.time() - start_time))}')
+# ax.grid()
+# fig.savefig(f"{platform}-load-{load}-test_duration-{test_duration}.png")
+# plt.show()
 
-print(f"{np.average(server_arrival_latency)}=")
-print(responses[0][0])
+# print(f"{np.average(server_arrival_latency)}=")
+# print(responses[0][0])
