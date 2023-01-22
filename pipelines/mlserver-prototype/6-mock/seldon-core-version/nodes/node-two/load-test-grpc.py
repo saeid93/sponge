10b3f85608b8d08299e509483945e7b804c85522
Saeid Ghafouri <s.ghafouri@qmul.ac.uk>
2023-01-22 01:26:13 +0000

    before solving the new bug of even distribution

--- pipelines/mlserver-prototype/6-mock/seldon-core-version/nodes/node-two/load-test-grpc.py
+++ pipelines/mlserver-prototype/6-mock/seldon-core-version/nodes/node-two/load-test-grpc.py
@@ -2,47 +2,32 @@ from barazmoon import MLServerAsyncGrpc
 from barazmoon import Data
 import asyncio
 import time
-import os
-import pathlib
-from PIL import Image
 import numpy as np
 
-def image_loader(folder_path, image_name):
-    image = Image.open(
-        os.path.join(folder_path, image_name))
-    # if there was a need to filter out only color images
-    # if image.mode == 'RGB':
-    #     pass
-    return image
-
-load = 100
+load = 1
 test_duration = 5
 variant = 0
 platform = 'seldon'
-image_name = 'input-sample.JPEG'
-workload = [load] * test_duration
-data_type = 'image'
-mode = 'equal' # options - step, equal, exponential
+mode = 'equal'
 
-PATH = pathlib.Path(__file__).parent.resolve()
-data = image_loader(PATH, 'input-sample.JPEG')
-data_shape = list(np.array(data).shape)
-data = np.array(data).flatten()
+request = {
+    'times': {
+        'models': {
+            'audio': {
+                'arrival': 1672276157.286681,
+                'serving': 1672276157.2869108
+                }
+            }
+        },
+    'model_name': 'nlp-qa',
+    'outputs': [{'data': 'mister quilter is the apostle of the middle classes and we are glad to welcome his gospel'}]
+}
 
-# single node inference
-if platform == 'seldon':
-    endpoint = "localhost:32000"
-    deployment_name = 'resnet-human'
-    model = 'resnet-human'
-    namespace = "default"
-    metadata = [("seldon", deployment_name), ("namespace", namespace)]
-elif platform == 'mlserver':
-    endpoint = "localhost:8081"
-    model = 'resnet-human'
-    metadata = []
 
-times = '["{\'node-one\': {\'arrival\': 1672276157.286681, \'serving\': 1672276157.2869108}}"]'
+times = str([str(request['times']['models'])])
+data = request['outputs'][0]['data']
 
+data_shape = [1]
 custom_parameters = {'times': str(times)}
 data_1 = Data(
     data=data,
@@ -50,9 +35,21 @@ data_1 = Data(
     custom_parameters=custom_parameters
 )
 
-# Data list
-data = []
-data.append(data_1)
+# single node inference
+if platform == 'seldon':
+    endpoint = "localhost:32000"
+    deployment_name = 'node-two'
+    model = 'node-two'
+    namespace = "default"
+    metadata = [("seldon", deployment_name), ("namespace", namespace)]
+elif platform == 'mlserver':
+    endpoint = "localhost:8081"
+    model = 'nlp-qa'
+    metadata = []
+
+workload = [load] * test_duration
+data_shape = [len(data)]
+data_type = 'text'
 
 start_time = time.time()
 
@@ -61,11 +58,10 @@ load_tester = MLServerAsyncGrpc(
     metadata=metadata,
     workload=workload,
     model=model,
-    mode=mode,
-    data=data,
+    data=[data_1],
+    mode=mode, # options - step, equal, exponential
     data_shape=data_shape,
-    data_type=data_type,
-    benchmark_duration=1)
+    data_type=data_type)
 
 responses = asyncio.run(load_tester.start())
 
@@ -73,8 +69,6 @@ print(f'{(time.time() - start_time):2.2}s spent in total')
 
 import matplotlib.pyplot as plt
 import numpy as np
-
-# Through away initial seconds results
 
 # # roundtrip latency
 # roundtrip_lat = []
@@ -87,7 +81,7 @@ import numpy as np
 # ax.plot(np.arange(len(roundtrip_lat)), roundtrip_lat)
 # ax.set(xlabel='request id', ylabel='roundtrip latency (s)', title=f'roundtrip latency, total time={round((time.time() - start_time))}')
 # ax.grid()
-# fig.savefig(f"grpc-compressed-image-{platform}_variant_{variant}-roundtrip_lat-load-{load}-test_duration-{test_duration}.png")
+# fig.savefig(f"grpc-compressed-audio-{platform}_variant_{variant}-roundtrip_lat-load-{load}-test_duration-{test_duration}.png")
 # plt.show()
 
 # # sending time
@@ -101,7 +95,7 @@ import numpy as np
 # ax.plot(np.arange(len(start_times)), start_times)
 # ax.set(xlabel='request id', ylabel='sending time (s)', title=f'load tester sending time, total time={round((time.time() - start_time))}')
 # ax.grid()
-# fig.savefig(f"grpc-compressed-image-{platform}_variant_{variant}-sending_time-load-{load}-test_duration-{test_duration}.png")
+# fig.savefig(f"grpc-compressed-audio-{platform}_variant_{variant}-sending_time-load-{load}-test_duration-{test_duration}.png")
 # plt.show()
 
 # # server arrival time
@@ -115,9 +109,10 @@ import numpy as np
 # ax.plot(np.arange(len(server_arrival_time)), server_arrival_time)
 # ax.set(xlabel='request id', ylabel='server arrival time (s)', title=f'Server recieving time of requests, total time={round((time.time() - start_time))}')
 # ax.grid()
-# fig.savefig(f"grpc-compressed-image-{platform}_variant_{variant}-server_arrival_time_from_start-load-{load}-test_duration-{test_duration}.png")
+# fig.savefig(f"grpc-compressed-audio-{platform}_variant_{variant}-server_arrival_time_from_start-load-{load}-test_duration-{test_duration}.png")
 # plt.show()
 
+model = 'nlp-qa'
 # server arrival latency
 server_arrival_latency = []
 for sec_resps in responses:
@@ -125,11 +120,12 @@ for sec_resps in responses:
         times = resp['times']
         server_recieving_time = times['models'][model]['arrival'] - times['request']['sending']
         server_arrival_latency.append(server_recieving_time)
-fig, ax = plt.subplots()
-ax.plot(np.arange(len(server_arrival_latency)), server_arrival_latency)
-ax.set(xlabel='request id', ylabel='server arrival latency (s)', title=f'Server recieving latency, total time={round((time.time() - start_time))}')
-ax.grid()
-fig.savefig(f"grpc-compressed-image-{platform}_variant_{variant}-server_recieving_latency-load-{load}-test_duration-{test_duration}.png")
-plt.show()
+# fig, ax = plt.subplots()
+# ax.plot(np.arange(len(server_arrival_latency)), server_arrival_latency)
+# ax.set(xlabel='request id', ylabel='server arrival latency (s)', title=f'Server recieving latency, total time={round((time.time() - start_time))}')
+# ax.grid()
+# fig.savefig(f"custom-{platform}-load-{load}-test_duration-{test_duration}.png")
+# plt.show()
 
 print(f"{np.average(server_arrival_latency)}=")
+print(responses[0][0])
