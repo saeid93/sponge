f38fe94d2e293bca1e043b6501982fec871d123f
Saeid Ghafouri <s.ghafouri@qmul.ac.uk>
2023-03-04 21:08:20 +0000

    minimal example to fix throughptu

--- simulator/minimal_example.py
+++ simulator/minimal_example.py
@@ -4,18 +4,20 @@ model = gp.Model()
 
 x = model.addVar(vtype=gp.GRB.BINARY, name="x")
 y = model.addVar(vtype=gp.GRB.BINARY, name="y")
+a = model.addVar(vtype=gp.GRB.BINARY, name="a")
 
-z = model.addVar(vtype=gp.GRB.CONTINUOUS, name="z")
+
+z = model.addVar(vtype=gp.GRB.CONTINUOUS, name="z", ub=30)
+model.addGenConstrAnd(a, [x, y], "andconstr")
 
 model.update()
-constr = model.addConstr((x + y == 2) >> (z <= 20))
 
-model.setObjective(z, gp.GRB.MAXIMIZE)
+# constr1 = model.addConstr(z <= 20)
+constr2 = model.addConstr((a == 1) >> (z <= 5))
 
+model.setObjective(z+x+y, gp.GRB.MAXIMIZE)
+
+model.update()
 # Solve the model
 model.optimize()
-
-# Print the solution
-print(f"x = {x.x}")
-print(f"y = {y.x}")
-print(f"z = {z.x}")
+model.display()
