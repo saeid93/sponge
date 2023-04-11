8d55c4e81db47175f44c7310042cbc3cd525cf12
Saeid Ghafouri <s.ghafouri@qmul.ac.uk>
2023-04-11 01:30:34 +0000

    mock models

---
+++ pipelines/mlserver-final/mock/seldon-core-version/nodes/batch/models.py
@@ -0,0 +1,50 @@
+import os
+import time
+from mlserver import MLModel
+import numpy as np
+from mlserver.logging import logger
+from mlserver.types import (
+    InferenceRequest,
+    InferenceResponse,
+    ResponseOutput,
+    Parameters)
+from mlserver import MLModel
+from typing import List
+
+try:
+    PREDICTIVE_UNIT_ID = os.environ['PREDICTIVE_UNIT_ID']
+    logger.info(f'PREDICTIVE_UNIT_ID set to: {PREDICTIVE_UNIT_ID}')
+except KeyError as e:
+    PREDICTIVE_UNIT_ID = 'mock'
+    logger.info(
+        f"PREDICTIVE_UNIT_ID env variable not set, using default value: {PREDICTIVE_UNIT_ID}")
+
+def decode_from_bin(
+    inputs: List[bytes], shapes: List[
+        List[int]], dtypes: List[str]) -> List[np.array]:
+    batch = []
+    for input, shape, dtype in zip(inputs, shapes, dtypes):
+        buff = memoryview(input)
+        array = np.frombuffer(buff, dtype=dtype).reshape(shape)
+        batch.append(array)
+    return batch
+
+def model(inputs: List[np.ndarray]):
+    logger.info(f'unprocessed inputs: {inputs}')
+    return list(map(lambda l: {'text': 'text'}, inputs))
+
+class Batch(MLModel):
+    async def load(self):
+        self.loaded = False
+        self.request_counter = 0
+        self.batch_counter = 0
+        self.model = model
+        self.loaded = True
+        return self.loaded
+
+    async def predict(self, payload: InferenceRequest) -> InferenceResponse:
+        payload = InferenceResponse(
+            outputs=payload.inputs,
+            model_name=self.name,
+        )
+        return payload
