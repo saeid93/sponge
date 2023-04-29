9942909581fdaf827eb1e77e8c111c143583e2a6
Saeid Ghafouri <s.ghafouri@qmul.ac.uk>
2023-04-29 14:19:59 +0000

    updates

--- experiments/runner/run.sh
+++ experiments/runner/run.sh
@@ -5,6 +5,6 @@
 # python runner_script.py --config-name video-5
 # python runner_script.py --config-name video-6
 # python runner_script.py --config-name video-7
-python runner_script.py --config-name video-8
-sleep 20
+# python runner_script.py --config-name video-8
+# sleep 20
 python runner_script.py --config-name video-9
