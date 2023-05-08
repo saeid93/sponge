718e1db26f00f01dcc5059360b763648acafd337
Saeid Ghafouri <s.ghafouri@qmul.ac.uk>
2023-05-08 17:24:57 +0000

    added backup scripts

---
+++ data/download.sh
@@ -0,0 +1,2 @@
+# considering that you have already connected to the object storage
+gsutil cp -r gs://ipa-results/results .
---
+++ data/upload.sh
@@ -0,0 +1,2 @@
+# considering that you have already connected to the object storage
+gsutil cp -r ./results gs://ipa-results/results
