fb99756e905b40fee9d48fc0e88936414eb009d7
Saeid Ghafouri <s.ghafouri@qmul.ac.uk>
2023-05-12 00:08:09 +0000

    experiments comparisons added

--- experiments/utils/drawing.py
+++ experiments/utils/drawing.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+import numpy as np
 from typing import Dict, List
 
 def draw_temporal(dict_to_draw: Dict[str, List[int]], adaptation_interval=None, multiple_experiments=False):
@@ -49,17 +50,41 @@ def draw_temporal(dict_to_draw: Dict[str, List[int]], adaptation_interval=None, 
     plt.tight_layout()
     plt.show()
 
-def draw_cumulative(dict_to_draw: Dict[str, List[int]]):
-    num_keys = len(dict_to_draw.keys())
-    dict_to_draw_cul = {key: sum(value) for key, value in dict_to_draw.items()}
-    fig, axs = plt.subplots(figsize=(6, 4))
-    x_values = list(dict_to_draw_cul.keys())
-    y_values = list(dict_to_draw_cul.values())
+def draw_cumulative(dict_to_draw: Dict[str, List[int]], multiple_experiments=False):
+    if not multiple_experiments:
+        dict_to_draw_cul = {key: sum(value) for key, value in dict_to_draw.items()}
+        fig, axs = plt.subplots(figsize=(4, 3))
+        x_values = list(dict_to_draw_cul.keys())
+        y_values = list(dict_to_draw_cul.values())
 
-    axs.bar(x_values, y_values)
-    axs.set_xlabel('Models')
+        axs.bar(x_values, y_values)
+        axs.set_xlabel('Models')
 
-    axs.set_xticklabels(x_values)
+        axs.set_xticklabels(x_values)
+    else:
+        dict_to_draw_cul = {}
+        for series, series_dict in dict_to_draw.items():
+            dict_to_draw_cul[series] = {key: sum(
+                list(filter(lambda x: x is not None, value))) for key, value in series_dict.items()}
+        fig, axs = plt.subplots(figsize=(4, 3))
+        experiments = list(dict_to_draw_cul.keys())
+        model_names = list(dict_to_draw_cul[experiments[0]].keys())
+        num_experiments = len(experiments)
+
+        bar_width = 1 / (num_experiments + 1)
+        bar_positions = np.arange(len(model_names))
+
+        for i, experiment in enumerate(experiments):
+            y_values = list(dict_to_draw_cul[experiment].values())
+            x_positions = bar_positions + i * bar_width
+            axs.bar(x_positions, y_values, width=bar_width, label=str(experiment))
+
+        axs.set_xlabel('Models')
+        axs.set_ylabel('Values')
+        axs.set_title('Comparison of Experiments')
+        axs.set_xticks(bar_positions + bar_width * (num_experiments - 1) / 2)
+        axs.set_xticklabels(model_names)
+        axs.legend(title='Experiments')
 
     plt.tight_layout()
     plt.show()
