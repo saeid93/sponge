1a64418afe11be5f2cd124064c17cb1cf728a5c0
mehran <mehransi95@gmail.com>
2023-06-06 17:09:34 +0330

    Add plot for workload patterns

---
+++ experiments/runner/trace.py
@@ -0,0 +1,171 @@
+import numpy as np
+import tensorflow as tf
+import matplotlib.pyplot as plt
+import os
+import sys
+from tensorflow.keras.models import load_model
+from barazmoon.twitter import twitter_workload_generator
+from statsmodels.tsa.arima.model import ARIMA
+
+
+project_dir = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
+sys.path.append(os.path.normpath(os.path.join(project_dir, "..", "..")))
+
+
+LSTM_PATH = os.path.join(project_dir, "data/lstm")
+lstm_plot_kwargs = {"label": "LSTM",}
+arima_plot_kwargs = {"label": "ARIMA",}
+
+
+lstm = load_model(LSTM_PATH)
+fig, axs = plt.subplots(2, 2, figsize=(12, 4.5))
+for axes in axs:
+    for ax in axes:
+        ax.set_ylim([0, 57])
+
+def get_x_y(data):
+    x = []
+    y = []
+    input_length = 12
+    data = data[:]
+    for i in range(input_length):
+        data.insert(0, data[0])
+    for i in range(0, len(data) - input_length):
+        x.extend(data[i : i + input_length])
+        y.append(data[i + input_length])
+    x = np.array(x)
+    print("shapes")
+    print(x.shape)
+    print(len(y))
+    return tf.convert_to_tensor(x.reshape((-1, input_length, 1)), dtype=tf.float32), y
+
+
+
+def get_arima_prediction(data):
+    preds = []
+    data = np.array(data).reshape((-1, 12))
+    for history in data:
+        model = ARIMA(history, order=(1, 0, 0))
+        model_fit = model.fit()
+        pred = int(max(model_fit.forecast(steps=2)))  # max
+        preds.append(pred)
+    return preds
+
+
+
+start = 1296000
+duration = 20 * 60  # 20 minutes
+workload = twitter_workload_generator(f"{0}-{21*24*60*60}", damping_factor=5)
+while True:
+    selected_workload = workload[start : start + duration]
+    if (
+        max(selected_workload) - min(selected_workload) > 30
+        and abs(selected_workload[0] - selected_workload[-200]) < 5
+        and abs(selected_workload[0] - selected_workload[500]) > 30
+    ):
+        break
+    start += 60 * 2
+
+
+
+ax = axs[0][0]
+ax.plot(list(range(len(selected_workload))), selected_workload, label="Real")
+x, _ = get_x_y(selected_workload)
+ax.plot(list(range(len(selected_workload))), list(lstm.predict(x)), **lstm_plot_kwargs)
+arima = get_arima_prediction(x)
+ax.plot(list(range(len(selected_workload))), arima, **arima_plot_kwargs)
+print(f"start: {start}")  # 1301160
+print(f"end: {start+duration}")
+
+
+
+start = 1296000
+duration = 20 * 60  # 10 minutes
+while True:
+    selected_workload = workload[start : start + duration]
+    if (
+        max(selected_workload) - min(selected_workload) < 9
+        and max(selected_workload) < 20
+    ):
+        break
+    start += duration
+
+
+ax2 = axs[0][1]
+ax2.plot(list(range(len(selected_workload))), selected_workload, label="Real")
+x, _ = get_x_y(selected_workload)
+ax2.plot(list(range(len(selected_workload))), list(lstm.predict(x)), **lstm_plot_kwargs)
+arima = get_arima_prediction(x)
+ax2.plot(list(range(len(selected_workload))), arima, **arima_plot_kwargs)
+print(f"start: {start}")  # 1299600
+print(f"end: {start+duration}")
+
+
+
+start = 1296000
+duration = 10 * 60  # 10 minutes
+while True:
+    selected_workload = workload[start : start + duration]
+    if (
+        max(selected_workload) - min(selected_workload) < 18
+        and min(selected_workload) > 30
+    ):
+        break
+    start += duration
+
+
+ax3 = axs[1][0]
+selected_workload = selected_workload * 2
+ax3.plot(list(range(len(selected_workload))), selected_workload, label="Real")
+x, _ = get_x_y(selected_workload)
+ax3.plot(list(range(len(selected_workload))), list(lstm.predict(x)), **lstm_plot_kwargs)
+arima = get_arima_prediction(x)
+ax3.plot(list(range(len(selected_workload))), arima, **arima_plot_kwargs)
+print(f"start: {start}")  # 1768800
+print(f"end: {start+duration}")
+
+
+
+start = 1296000
+duration = 10 * 60  # 10 minutes
+while True:
+    selected_workload = workload[start : start + duration]
+    if max(selected_workload) - min(selected_workload) > 30:
+        break
+    start += duration
+
+start2 = start + duration
+while True:
+    selected_workload2 = workload[start2 : start2 + duration]
+    if max(selected_workload2) - min(selected_workload2) > 30:
+        break
+    start2 += duration
+selected_workload = selected_workload + selected_workload2
+
+
+ax4 = axs[1][1]
+ax4.plot(list(range(len(selected_workload))), selected_workload, label="Real")
+x, _ = get_x_y(selected_workload)
+ax4.plot(list(range(len(selected_workload))), list(lstm.predict(x)), **lstm_plot_kwargs)
+arima = get_arima_prediction(x)
+ax4.plot(list(range(len(selected_workload))), arima, **arima_plot_kwargs)
+
+plt.legend(
+    fontsize=13,
+    fancybox=False,
+    ncol=3,
+    frameon=False,
+    bbox_to_anchor=(0.27,2.5),
+    handlelength=1,
+    columnspacing=0.8
+)
+fig.text(0.51, -0.02, 'Time (s)', ha='center')
+fig.text(0.053, 0.5, "Workload (RPS)", rotation=90, va='center')
+plt.savefig(
+    os.path.dirname(__file__) + "/trace-figures/patterns.png",
+    dpi=600,
+    format="png",
+    bbox_inches="tight",
+    pad_inches=0,
+)
+print(f"start: {start} | start2: {start2}")  # start: 1301400 | start2: 1308600
