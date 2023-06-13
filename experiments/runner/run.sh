a0ff32746d98e467a6b27668f9fb76c657869a6c
Saeid Ghafouri <s.ghafouri@qmul.ac.uk>
2023-06-13 03:22:25 +0000

    changes

--- experiments/runner/run.sh
+++ experiments/runner/run.sh
@@ -1,19 +1,26 @@
 # python runner_script.py --config-name audio-sent-3-old
 # sleep 90
-python runner_script.py --config-name video-1
-sleep 60
-python runner_script.py --config-name video-2
-sleep 60
-python runner_script.py --config-name video-3
-sleep 60
-python runner_script.py --config-name audio-qa-1
-sleep 60
-python runner_script.py --config-name audio-qa-2
-sleep 60
-python runner_script.py --config-name audio-qa-3
-sleep 60
-python runner_script.py --config-name audio-sent-1
-sleep 60
-python runner_script.py --config-name audio-sent-2
-sleep 60
-python runner_script.py --config-name audio-sent-3
+# python runner_script.py --config-name video-1
+# sleep 60
+# python runner_script.py --config-name video-2
+# sleep 60
+# python runner_script.py --config-name video-3
+# sleep 60
+# python runner_script.py --config-name audio-qa-1
+# sleep 60
+# python runner_script.py --config-name audio-qa-2
+# sleep 60
+# python runner_script.py --config-name audio-qa-3
+# sleep 60
+# python runner_script.py --config-name audio-sent-1
+# sleep 60
+# python runner_script.py --config-name audio-sent-2
+# sleep 60
+# python runner_script.py --config-name audio-sent-3
+# python runner_script.py --config-name 1000-video
+# sleep 60
+# python runner_script.py --config-name 1001-video
+# sleep 60
+# python runner_script.py --config-name 1002-video
+# sleep 60
+# python runner_script.py --config-name 1004-video
