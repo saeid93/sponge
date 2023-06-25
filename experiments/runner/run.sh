8fb81aa43b4bd086980d3f20d970f20b2d71a938
Saeid Ghafouri <s.ghafouri@qmul.ac.uk>
2023-06-25 03:16:23 +0000

    updated to test batching

--- experiments/runner/run.sh
+++ experiments/runner/run.sh
@@ -1,35 +1,35 @@
 # python runner_script.py --config-name audio-sent-3-old
 # sleep 90
-python runner_script.py --config-name video-1
-sleep 60
-python runner_script.py --config-name video-2
-sleep 60
-python runner_script.py --config-name video-3
-sleep 60
-python runner_script.py --config-name video-4
-sleep 60
-python runner_script.py --config-name video-5
-sleep 60
-python runner_script.py --config-name video-6
-sleep 60
-python runner_script.py --config-name video-7
-sleep 60
-python runner_script.py --config-name video-8
-sleep 60
-python runner_script.py --config-name video-9
-sleep 60
-python runner_script.py --config-name video-10
-sleep 60
-python runner_script.py --config-name video-11
-sleep 60
-python runner_script.py --config-name video-12
-sleep 60
-python runner_script.py --config-name video-13
-sleep 60
-python runner_script.py --config-name video-14
-sleep 60
-python runner_script.py --config-name video-15
-sleep 60
+# python runner_script.py --config-name video-1
+# sleep 60
+# python runner_script.py --config-name video-2
+# sleep 60
+# python runner_script.py --config-name video-3
+# sleep 60
+# python runner_script.py --config-name video-4
+# sleep 60
+# python runner_script.py --config-name video-5
+# sleep 60
+# python runner_script.py --config-name video-6
+# sleep 60
+# python runner_script.py --config-name video-7
+# sleep 60
+# python runner_script.py --config-name video-8
+# sleep 60
+# python runner_script.py --config-name video-9
+# sleep 60
+# python runner_script.py --config-name video-10
+# sleep 60
+# python runner_script.py --config-name video-11
+# sleep 60
+# python runner_script.py --config-name video-12
+# sleep 60
+# python runner_script.py --config-name video-13
+# sleep 60
+# python runner_script.py --config-name video-14
+# sleep 60
+# python runner_script.py --config-name video-15
+# sleep 60
 python runner_script.py --config-name video-16
 sleep 60
 python runner_script.py --config-name video-17
@@ -39,17 +39,6 @@ sleep 60
 python runner_script.py --config-name video-19
 sleep 60
 python runner_script.py --config-name video-20
-# python runner_script.py --config-name audio-qa-1
-# sleep 60
-# python runner_script.py --config-name audio-qa-2
-# sleep 60
-# python runner_script.py --config-name audio-qa-3
-# sleep 60
-# python runner_script.py --config-name audio-sent-1
-# sleep 60
-# python runner_script.py --config-name audio-sent-2
-# sleep 60
-# python runner_script.py --config-name audio-sent-3
 
 # python runner_script.py --config-name audio-qa-1
 # sleep 60
@@ -90,7 +79,7 @@ python runner_script.py --config-name video-20
 # python runner_script.py --config-name audio-qa-19
 # sleep 60
 # python runner_script.py --config-name audio-qa-20
-
+# sleep 60
 # python runner_script.py --config-name audio-sent-1
 # sleep 60
 # python runner_script.py --config-name audio-sent-2
