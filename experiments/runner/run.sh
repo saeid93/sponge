9468960f98a6b1830a59e52af244e4ce1f4cfbfd
Saeid Ghafouri <s.ghafouri@qmul.ac.uk>
2023-06-26 03:18:16 +0000

    updated figures

--- experiments/runner/run.sh
+++ experiments/runner/run.sh
@@ -39,86 +39,88 @@ sleep 60
 python runner_script.py --config-name video-19
 sleep 60
 python runner_script.py --config-name video-20
+sleep 60
 
-# python runner_script.py --config-name audio-qa-1
-# sleep 60
-# python runner_script.py --config-name audio-qa-2
-# sleep 60
-# python runner_script.py --config-name audio-qa-3
-# sleep 60
-# python runner_script.py --config-name audio-qa-4
-# sleep 60
-# python runner_script.py --config-name audio-qa-5
-# sleep 60
-# python runner_script.py --config-name audio-qa-6
-# sleep 60
-# python runner_script.py --config-name audio-qa-7
-# sleep 60
-# python runner_script.py --config-name audio-qa-8
-# sleep 60
-# python runner_script.py --config-name audio-qa-9
-# sleep 60
-# python runner_script.py --config-name audio-qa-10
-# sleep 60
-# python runner_script.py --config-name audio-qa-11
-# sleep 60
-# python runner_script.py --config-name audio-qa-12
-# sleep 60
-# python runner_script.py --config-name audio-qa-13
-# sleep 60
-# python runner_script.py --config-name audio-qa-14
-# sleep 60
-# python runner_script.py --config-name audio-qa-15
-# sleep 60
-# python runner_script.py --config-name audio-qa-16
-# sleep 60
-# python runner_script.py --config-name audio-qa-17
-# sleep 60
-# python runner_script.py --config-name audio-qa-18
-# sleep 60
-# python runner_script.py --config-name audio-qa-19
-# sleep 60
-# python runner_script.py --config-name audio-qa-20
-# sleep 60
-# python runner_script.py --config-name audio-sent-1
-# sleep 60
-# python runner_script.py --config-name audio-sent-2
-# sleep 60
-# python runner_script.py --config-name audio-sent-3
-# sleep 60
-# python runner_script.py --config-name audio-sent-4
-# sleep 60
-# python runner_script.py --config-name audio-sent-5
-# sleep 60
-# python runner_script.py --config-name audio-sent-6
-# sleep 60
-# python runner_script.py --config-name audio-sent-7
-# sleep 60
-# python runner_script.py --config-name audio-sent-8
-# sleep 60
-# python runner_script.py --config-name audio-sent-9
-# sleep 60
-# python runner_script.py --config-name audio-sent-10
-# sleep 60
-# python runner_script.py --config-name audio-sent-11
-# sleep 60
-# python runner_script.py --config-name audio-sent-12
-# sleep 60
-# python runner_script.py --config-name audio-sent-13
-# sleep 60
-# python runner_script.py --config-name audio-sent-14
-# sleep 60
-# python runner_script.py --config-name audio-sent-15
-# sleep 60
-# python runner_script.py --config-name audio-sent-16
-# sleep 60
-# python runner_script.py --config-name audio-sent-17
-# sleep 60
-# python runner_script.py --config-name audio-sent-18
-# sleep 60
-# python runner_script.py --config-name audio-sent-19
-# sleep 60
-# python runner_script.py --config-name audio-sent-20
+python runner_script.py --config-name audio-qa-1
+sleep 60
+python runner_script.py --config-name audio-qa-2
+sleep 60
+python runner_script.py --config-name audio-qa-3
+sleep 60
+python runner_script.py --config-name audio-qa-4
+sleep 60
+python runner_script.py --config-name audio-qa-5
+sleep 60
+python runner_script.py --config-name audio-qa-6
+sleep 60
+python runner_script.py --config-name audio-qa-7
+sleep 60
+python runner_script.py --config-name audio-qa-8
+sleep 60
+python runner_script.py --config-name audio-qa-9
+sleep 60
+python runner_script.py --config-name audio-qa-10
+sleep 60
+python runner_script.py --config-name audio-qa-11
+sleep 60
+python runner_script.py --config-name audio-qa-12
+sleep 60
+python runner_script.py --config-name audio-qa-13
+sleep 60
+python runner_script.py --config-name audio-qa-14
+sleep 60
+python runner_script.py --config-name audio-qa-15
+sleep 60
+python runner_script.py --config-name audio-qa-16
+sleep 60
+python runner_script.py --config-name audio-qa-17
+sleep 60
+python runner_script.py --config-name audio-qa-18
+sleep 60
+python runner_script.py --config-name audio-qa-19
+sleep 60
+python runner_script.py --config-name audio-qa-20
+sleep 60
+
+python runner_script.py --config-name audio-sent-1
+sleep 60
+python runner_script.py --config-name audio-sent-2
+sleep 60
+python runner_script.py --config-name audio-sent-3
+sleep 60
+python runner_script.py --config-name audio-sent-4
+sleep 60
+python runner_script.py --config-name audio-sent-5
+sleep 60
+python runner_script.py --config-name audio-sent-6
+sleep 60
+python runner_script.py --config-name audio-sent-7
+sleep 60
+python runner_script.py --config-name audio-sent-8
+sleep 60
+python runner_script.py --config-name audio-sent-9
+sleep 60
+python runner_script.py --config-name audio-sent-10
+sleep 60
+python runner_script.py --config-name audio-sent-11
+sleep 60
+python runner_script.py --config-name audio-sent-12
+sleep 60
+python runner_script.py --config-name audio-sent-13
+sleep 60
+python runner_script.py --config-name audio-sent-14
+sleep 60
+python runner_script.py --config-name audio-sent-15
+sleep 60
+python runner_script.py --config-name audio-sent-16
+sleep 60
+python runner_script.py --config-name audio-sent-17
+sleep 60
+python runner_script.py --config-name audio-sent-18
+sleep 60
+python runner_script.py --config-name audio-sent-19
+sleep 60
+python runner_script.py --config-name audio-sent-20
 
 # python simulation_runner.py --config-name nlp-1000
 # python simulation_runner.py --config-name nlp-2000
