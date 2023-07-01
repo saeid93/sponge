c100079b1cb02aac8e67cec8671f608adb5334f7
Saeid Ghafouri <s.ghafouri@qmul.ac.uk>
2023-07-01 20:36:12 +0000

    fixed the bug in the final resutls

--- experiments/profiling/single-node/run.sh
+++ experiments/profiling/single-node/run.sh
@@ -4,7 +4,7 @@
 # python model_iter.py --config-name 2-config-sent
 # python model_iter.py --config-name 3-config-li
 # python model_iter.py --config-name 3-config-sum
-python model_iter.py --config-name 3-config-trans
+# python model_iter.py --config-name 3-config-trans
 # python model_iter.py --config-name 4-config-qa
 # python model_iter.py --config-name 4-config-sum
 # python model_iter.py --config-name 5-config-resnet-human
