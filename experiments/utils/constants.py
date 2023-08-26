dc0ed7ed61b39c6e22120dec125899289e1b2512
Saeid Ghafouri <s.ghafouri@qmul.ac.uk>
2023-08-26 19:08:48 -0400

    adding gpu prototypes template

--- experiments/utils/constants.py
+++ experiments/utils/constants.py
@@ -2,7 +2,7 @@ import os
 from .obj import setup_obj_store
 
 # defined by the user
-PROJECT_PATH = "/home/cc/infernece-pipeline-joint-optimization"
+PROJECT_PATH = "/home/cc/ipa-private"
 OBJ_PATH = "/home/cc/my_mounting_point/"
 KEY_CONFIG_FILENAME = "key_config_mapper.csv"
 NAMESPACE = "default"
@@ -71,6 +71,9 @@ OBJ_PIPELINE_PROFILING_RESULTS_PATH = os.path.join(
 TEMP_MODELS_PATH = os.path.join(DATA_PATH, "model-temp")
 DATASETS = os.path.join(DATA_PATH, "datasets")
 
+# accuracy evaluation results folder
+ACCURACIES_EVAL = os.path.join(DATA_PATH, "accuracy-evaluation")
+
 
 def create_dirs():
     """
@@ -119,7 +122,8 @@ def create_dirs():
         os.makedirs(OBJ_PIPELINE_PROFILING_RESULTS_PATH)
     if not os.path.exists(DATASETS):
         os.makedirs(DATASETS)
-
+    if not os.path.exists(ACCURACIES_EVAL):
+        os.makedirs(ACCURACIES_EVAL)
 
 # prometheus client
 PROMETHEUS = "http://localhost:30090"
