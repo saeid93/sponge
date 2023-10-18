230cfac9e8d94f7e254f32695959fbb8469842ea
Saeid Ghafouri <s.ghafouri@qmul.ac.uk>
2023-10-18 20:06:29 +0000

    mult video configs added

---
+++ experiments/runner/run-lowest-real.sh
@@ -0,0 +1,41 @@
+python runner_script.py --config-name mul-video-1
+sleep 60
+python runner_script.py --config-name mul-video-2
+sleep 60
+python runner_script.py --config-name mul-video-3
+sleep 60
+python runner_script.py --config-name mul-video-4
+sleep 60
+python runner_script.py --config-name mul-video-5
+sleep 60
+python runner_script.py --config-name mul-video-6
+sleep 60
+python runner_script.py --config-name mul-video-7
+sleep 60
+python runner_script.py --config-name mul-video-8
+sleep 60
+python runner_script.py --config-name mul-video-9
+sleep 60
+python runner_script.py --config-name mul-video-10
+sleep 60
+python runner_script.py --config-name mul-video-11
+sleep 60
+python runner_script.py --config-name mul-video-12
+sleep 60
+python runner_script.py --config-name mul-video-13
+sleep 60
+python runner_script.py --config-name mul-video-14
+sleep 60
+python runner_script.py --config-name mul-video-15
+sleep 60
+python runner_script.py --config-name mul-video-16
+sleep 60
+python runner_script.py --config-name mul-video-17
+sleep 60
+python runner_script.py --config-name mul-video-18
+sleep 60
+python runner_script.py --config-name mul-video-19
+sleep 60
+python runner_script.py --config-name mul-video-20
+sleep 60
+
