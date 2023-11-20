469d83a3af2367fb1e6af342b4f8aa04e9df5793
Saeid Ghafouri <s.ghafouri@qmul.ac.uk>
2023-11-20 18:14:58 +0000

    finalized the nlp mult - fixed burtsy

--- experiments/runner/run-uc.sh
+++ experiments/runner/run-uc.sh
@@ -661,35 +661,40 @@
 # python runner_script.py --config-name audio-qa-16-7
 # sleep 60
 
+# python runner_script.py --config-name nlp-1
+# sleep 60
+# python runner_script.py --config-name nlp-2
+# sleep 60
+# python runner_script.py --config-name nlp-3
+# sleep 60
+# python runner_script.py --config-name nlp-5
+# sleep 60
+# python runner_script.py --config-name nlp-6
+# sleep 60
+# python runner_script.py --config-name nlp-7
+# sleep 60
+# python runner_script.py --config-name nlp-8
+# sleep 60
+# python runner_script.py --config-name nlp-10
+# sleep 60
+# python runner_script.py --config-name nlp-11
+# sleep 60
+# python runner_script.py --config-name nlp-12
+# sleep 60
+# python runner_script.py --config-name nlp-13
+# sleep 60
+# python runner_script.py --config-name nlp-15
+# sleep 60
+# python runner_script.py --config-name nlp-16
+# sleep 60
+# python runner_script.py --config-name nlp-17
+# sleep 60
+# python runner_script.py --config-name nlp-18
+# sleep 60
+# python runner_script.py --config-name nlp-20
+# sleep 60
+
 python runner_script.py --config-name nlp-1
 sleep 60
-python runner_script.py --config-name nlp-2
-sleep 60
 python runner_script.py --config-name nlp-3
-sleep 60
-python runner_script.py --config-name nlp-5
-sleep 60
-python runner_script.py --config-name nlp-6
-sleep 60
-python runner_script.py --config-name nlp-7
-sleep 60
-python runner_script.py --config-name nlp-8
-sleep 60
-python runner_script.py --config-name nlp-10
-sleep 60
-python runner_script.py --config-name nlp-11
-sleep 60
-python runner_script.py --config-name nlp-12
-sleep 60
-python runner_script.py --config-name nlp-13
-sleep 60
-python runner_script.py --config-name nlp-15
-sleep 60
-python runner_script.py --config-name nlp-16
-sleep 60
-python runner_script.py --config-name nlp-17
-sleep 60
-python runner_script.py --config-name nlp-18
-sleep 60
-python runner_script.py --config-name nlp-20
 sleep 60
