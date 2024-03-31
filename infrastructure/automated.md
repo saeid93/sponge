9ce631806eb4ef398a50a017f0f9af4062e29f7c
Saeid Ghafouri <s.ghafouri@qmul.ac.uk>
2024-03-31 04:42:41 +0000

    installed poetry separate step added

--- infrastructure/automated.md
+++ infrastructure/automated.md
@@ -2,5 +2,6 @@
 
 1. `make zsh` to install zshell; reply 'Y' to setting zsh as the default
 2. `make docker_installation` to install docker
-3. close the existing terminal and open a new one
-4. For cluster installation `make all K8SVERSION=1.27 PUBLIC_IP=<server's public IP>` to do the rest of the installation steps.
+3. `make install_poetry` to install docker
+4. close the existing terminal and open a new one
+5. For cluster installation `make all K8SVERSION=1.27 PUBLIC_IP=<server's public IP>` to do the rest of the installation steps.
---
+++ infrastructure/hack/install_poetry.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Install Poetry
+curl -sSL https://install.python-poetry.org | python3 -
+
+# Set the directory where Poetry is installed
+POETRY_INSTALL_DIR="$HOME/.poetry/bin"
+
+# Add the directory to PATH if not already present
+if [[ ":$PATH:" != *":$POETRY_INSTALL_DIR:"* ]]; then
+    echo "Adding Poetry directory to PATH..."
+    echo "export PATH=\"$POETRY_INSTALL_DIR:\$PATH\"" >> "$HOME/.bashrc"
+    source "$HOME/.bashrc"
+    echo "Poetry directory has been added to PATH."
+else
+    echo "Poetry directory is already in PATH."
+fi
